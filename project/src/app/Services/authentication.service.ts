f2d6ff8b5e29e77cdae8aebc6d466c20702d28ba
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-02 14:28:05 +0200

    updated authentication service

--- project/src/app/Services/authentication.service.ts
+++ project/src/app/Services/authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { DatabaseService } from './database.service';
 import { HttpClient } from '@angular/common/http';
 import { ɵcamelCaseToDashCase } from '@angular/animations/browser';
-import { catchError, throwError } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -14,7 +14,8 @@ export class AuthenticationService {
   private domain: string = "http://localhost:8000"
   constructor(
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private dbService: DatabaseService
   ) { }
 
 
@@ -33,26 +34,56 @@ export class AuthenticationService {
 
   }
 
-
-
-  signUp(email: string, password: string){
+  signUp(name: string, surname: string, email: string, password: string){
     const body = {
+      name: name,
+      surname: surname,
       email: email,
       password: password
     }
     return this.http.post(`${this.domain}/api/v1/signup`, body)
       .pipe(
         catchError((error: any) => {
-          return throwError(() => new Error(error))
+          return error
         })
       )
 
 
   }
 
+  setToken(token: string){
+    localStorage.setItem("token",token)
+    console.log("STORED: ", token)
+    this.router.navigate(['homepage'])
+  }
+
+
   logOut(){ 
     localStorage.removeItem("token")
-    this.router.navigate(['login'])
+    this.router.navigate(['authentication'])
   }
  
+  isAuthenticated(): Observable<boolean> {
+    const token = localStorage.getItem("token");
+    if (token) {
+      return this.verifyToken(token).pipe(
+        map((response: any) => response.valid),
+        catchError(() => of(false))
+      );
+    } else {
+      return of(false);
+    }
+  }
+  
+  verifyToken(token: string) {
+    return this.http.get(`${this.domain}/api/v1/token`, {
+      headers: { 'Authorization': `Bearer ${token}` }
+    }).pipe(
+      catchError((error: any) => {
+        console.log(error)
+        return error
+      })
+    );
+  }
+
 }
