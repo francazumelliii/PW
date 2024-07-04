0c20b67d8f0168f7c8ad1a1ddd199df63a8e2162
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-04 15:58:06 +0200

    dynamic list for card.component based on searchbar events

--- project/src/app/Components/homepage/homepage.component.ts
+++ project/src/app/Components/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
-import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { RoleService } from '../../Services/role.service';
 import { APIResponse, Restaurant } from '../../Interfaces/general';
+import { SweetalertService } from '../../Services/sweetalert.service';
+import { CardContainerComponent } from '../../Tools/card-container/card-container.component';
 
 @Component({
   selector: 'app-homepage',
@@ -10,10 +12,13 @@ import { APIResponse, Restaurant } from '../../Interfaces/general';
 export class HomepageComponent implements AfterContentInit{
   constructor(
     private roleService: RoleService,
+    private swal: SweetalertService,
+    private cdr: ChangeDetectorRef
   ){}
 
+  @ViewChild("cardContainer") cardContainer!: CardContainerComponent
   restaurantsList: Restaurant[] = [];
-
+  nearestRestaurantsList: Restaurant[] = [];
 
   ngAfterContentInit(): void {
     this.getAllRestaurants()
@@ -26,7 +31,32 @@ export class HomepageComponent implements AfterContentInit{
         console.log(response.data)
       },((error: any) => console.error(error)))
   }
-  handleLocation(event: GeolocationPosition){
-    console.log(event)
+  handleSearchbarLocation(event: any){
+    const latitude = event.lat
+    const longitude = event.lon
+    const county = event.address.state
+    this.roleService.getNearestRestaurants(latitude,longitude,county)
+      .subscribe((response: APIResponse) => {
+        if(response.success){
+          this.nearestRestaurantsList = response.data
+          this.cardContainer.list = this.nearestRestaurantsList
+          this.cardContainer.circular = false
+          this.cdr.detectChanges()
+        }
+    },((error: any) => {
+      this.swal.error("error","Error", error.toString(), "")
+    }))
+
   }
+
+  handleSearchbarClear(){
+    this.nearestRestaurantsList = []
+    this.cardContainer.list = this.restaurantsList
+    this.cardContainer.circular = true
+    this.cdr.detectChanges()
+  }
+
+
+
+
 }
