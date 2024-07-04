001e6ca1a7c09050cfc97994c8e91cfa19e02cf3
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-04 15:58:35 +0200

    added getNearestRestaurant declaration

--- project/src/app/Interfaces/delegate.ts
+++ project/src/app/Interfaces/delegate.ts
@@ -1,6 +1,9 @@
 import { Observable } from "rxjs";
 import { APIResponse, Restaurant } from "./general";
+import { ApiService } from "../Services/api.service";
 
 export interface Delegate {
     getAllRestaurants(): Observable<APIResponse>
+    getNearestRestaurants(lat: number | string, lon: number | string, county: string): Observable<APIResponse>
+    
 }
