1563f9a3b2901c71a5a3e4e3b3bb62674d174d0f
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-04 15:59:01 +0200

    new service for sweetalert modals

---
+++ project/src/app/Services/sweetalert.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SweetalertService {
+
+  constructor(
+  ) { }
+
+
+  error(icon: any = "error",title: any = null,text: string = "Something went wrong", footer: string = "", ){
+    Swal.fire({
+      icon: icon,
+      title: title,
+      text: text,
+      footer: footer
+    })
+  }
+}
