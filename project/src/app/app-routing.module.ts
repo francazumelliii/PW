586e3499b241abe24115a21976d35e65d3fbecbc
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-06 19:56:22 +0200

    new route for restaurant component

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { Component, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivateFn } from '@angular/router';
 import { HomepageComponent } from './Components/homepage/homepage.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { AuthenticationsComponent } from './Components/authentications/authentications.component';
 import { authGuard } from './Guards/auth-guard.guard';
+import { RestaurantComponent } from './Components/restaurant/restaurant.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "/authentication", pathMatch:"full"},
   {path: 'authentication', component: SidebarComponent,children: [{ path: '', component: AuthenticationsComponent }]},
-  {path: 'homepage', component: SidebarComponent,children: [{ path: '', component: HomepageComponent }], canActivate:[authGuard]}
+  {path: 'homepage', component: SidebarComponent,children: [{ path: '', component: HomepageComponent }], canActivate:[authGuard]},
+  {path: 'restaurant/:id', component: SidebarComponent,children: [{path: "",component: RestaurantComponent}], canActivate: [authGuard]}
 ];
 
 @NgModule({
