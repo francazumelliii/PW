4cb2caff2d251d54e13857bc791af9424d8742ee
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-10 17:48:19 +0200

    new(reservationFormGroup)

--- project/src/app/Services/form-control.service.ts
+++ project/src/app/Services/form-control.service.ts
@@ -1,5 +1,7 @@
+
+import { formatDate } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,11 @@ export class FormControlService {
 
   constructor(private fb: FormBuilder) {
     this.initLoginForm()
+    this.initReservationForm()
    }
 
    private loginFormGroup !: FormGroup;
-
+   private reservationFormGroup !: FormGroup;
 
    initLoginForm(){
     this.loginFormGroup = this.fb.group({
@@ -23,6 +26,19 @@ export class FormControlService {
    }
     get loginForm(){
     return this.loginFormGroup
+   }
+
+   initReservationForm(){
+    const today = formatDate(new Date(), "dd/MM/yyyy", "en-US")
+    this.reservationFormGroup = this.fb.group({
+      date: new FormControl(null, Validators.required),
+      turn: new FormControl(null,Validators.required),
+      quantity: new FormControl(null,[Validators.required,Validators.min(1), Validators.max(99)])
+    })
+   }
+
+   get reservationForm(){
+    return this.reservationFormGroup
    }
 
 
