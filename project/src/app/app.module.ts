b7069357c56416f4d2f57fa523444b9a3b19b62a
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-19 19:32:45 +0200

    update(imports and providers for modalService)

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpClientModule } from '@angular/common/http';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './Components/homepage/homepage.component';
@@ -11,25 +11,29 @@ import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { AuthenticationsComponent } from './Components/authentications/authentications.component';
 import { ButtonComponent } from './Tools/button/button.component';
 import { LoginFormComponent } from './Tools/login-form/login-form.component';
-import {MatList, MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { StepperModule } from 'primeng/stepper';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
-import {MatInputModule} from '@angular/material/input';
-// Import Angular Material modules
+import { MatInputModule } from '@angular/material/input';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; // Import MatToolbarModule
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { CardComponent } from './Tools/card/card.component';
 import { CardContainerComponent } from './Tools/card-container/card-container.component';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { SearchbarComponent } from './Components/searchbar/searchbar.component';
 import { MapComponent } from './Components/map/map.component';
 import { RestaurantComponent } from './Components/restaurant/restaurant.component';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
 import { ReservationComponent } from './Components/reservation/reservation.component';
+import { ModalComponent } from './Tools/modal/modal.component';
+import { ModalTestComponent } from './Tools/modal-test/modal-test.component';
+import { ModalService } from './Services/modal.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,6 +48,8 @@ import { ReservationComponent } from './Components/reservation/reservation.compo
     MapComponent,
     RestaurantComponent,
     ReservationComponent,
+    ModalComponent,
+    ModalTestComponent
   ],
   imports: [
     BrowserModule,
@@ -66,10 +72,8 @@ import { ReservationComponent } from './Components/reservation/reservation.compo
     MatSelectModule,
     MatInputModule
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'en-US'},],
-  bootstrap: [AppComponent]
+  providers: [{ provide: LOCALE_ID, useValue: 'en-US' }, ModalService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
-
-
-
