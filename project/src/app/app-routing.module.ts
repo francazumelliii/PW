11361d02674b6d22f2c75cd5f65cf7bb89670d91
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-07-20 12:56:56 +0200

    fix(removed sidebar from login Page)

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { AuthenticationsComponent } from './Components/authentications/authentications.component';
 import { authGuard } from './Guards/auth-guard.guard';
 import { RestaurantComponent } from './Components/restaurant/restaurant.component';
+import { AccountComponent } from './Components/account/account.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "/authentication", pathMatch:"full"},
-  {path: 'authentication', component: SidebarComponent,children: [{ path: '', component: AuthenticationsComponent }]},
+  {path: 'authentication', component: AuthenticationsComponent },
   {path: 'homepage', component: SidebarComponent,children: [{ path: '', component: HomepageComponent }], canActivate:[authGuard]},
-  {path: 'restaurant/:id', component: SidebarComponent,children: [{path: "",component: RestaurantComponent}], canActivate: [authGuard]}
+  {path: 'restaurant/:id', component: SidebarComponent,children: [{path: "",component: RestaurantComponent}], canActivate: [authGuard]},
+  {path: 'account', component: SidebarComponent,children: [{path: "",component: AccountComponent}], canActivate: [authGuard]}
 ];
 
 @NgModule({
