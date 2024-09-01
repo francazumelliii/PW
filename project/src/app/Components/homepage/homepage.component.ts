72162721f431c973420e88cc50ea156a50014487
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-09-01 19:15:51 +0200

    update(new patch reservationModal button)

--- project/src/app/Components/homepage/homepage.component.ts
+++ project/src/app/Components/homepage/homepage.component.ts
@@ -1,56 +1,84 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef, AfterContentInit, platformCore, ComponentRef, ComponentFactoryResolver } from '@angular/core';
 import { RoleService } from '../../Services/role.service';
-import { APIResponse, Images, Menu, Restaurant } from '../../Interfaces/general';
+import { APIResponse, Images, Menu, Reservation, Restaurant } from '../../Interfaces/general';
 import { SweetalertService } from '../../Services/sweetalert.service';
 import { MapComponent } from '../map/map.component';
 import { Router } from '@angular/router';
+import { ModalService } from '../../Services/modal.service';
+import { UpdateModalComponent } from '../../Tools/update-modal/update-modal.component';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.sass']
 })
-export class HomepageComponent implements AfterContentInit {
+export class HomepageComponent implements OnInit {
   restaurantsList: Restaurant[] = [];
   nearestRestaurantsList: Restaurant[] = [];
   showNearestRestaurants: boolean = false;
   images: Images[] = []
   menus: Menu[] = []
+  reservations: Reservation[] = []
 
   @ViewChild("map") map!: MapComponent;
-  circular:boolean = true
+  circular: boolean = true;
 
   constructor(
     public roleService: RoleService,
     private swal: SweetalertService,
     private cdr: ChangeDetectorRef,
-    private router: Router
+    private router: Router,
+    private modalService: ModalService,
+
   ){}
 
-  ngAfterContentInit(): void {
+  ngOnInit(): void {
     this.getAllRestaurants();
   }
-  
+
+
   getAllRestaurants() {
     this.roleService.getAllRestaurants().subscribe((response: APIResponse) => {
       if(response.success) {
         this.restaurantsList = [...response.data];
-        this.map ? this.map.list = response.data : null;
+        console.log(this.restaurantsList)
+        if (this.map) {
+          this.map.list = response.data;
+        }
         this.getRestaurantImages(+response.data[0].restaurant.id);
-        this.getAllMenus(+response.data[0].restaurant.id)
+        this.getRestaurantReservations(+response.data[0].restaurant.id);
+        this.getAllMenus(+response.data[0].restaurant.id);
+        this.showNearestRestaurants = false; // Reset to show all restaurants initially
+        this.circular = false; // Stop loading indicator
+        this.cdr.detectChanges();
       }
     }, (error: any) => {
       console.error(error);
     });
   }
-  getRestaurantImages(id: number){
-    this.roleService.getRestaurantImages(id)
-      .subscribe((response: APIResponse) => {
-        response.success ? this.images = response.data : null
-      }, (error : any) => {
-        console.error(error),
-        this.swal.fire("error","ERROR","Error retrieving data... try later", "")
-      })
+
+  getRestaurantImages(id: number) {
+    this.roleService.getRestaurantImages(id).subscribe((response: APIResponse) => {
+      if (response.success) {
+        this.images = response.data;
+        this.cdr.detectChanges();
+      }
+    }, (error: any) => {
+      console.error(error);
+      this.swal.fire("error", "ERROR", "Error retrieving data... try later", "");
+    });
+  }
+
+  getAllMenus(id: number) {
+    this.roleService.getAllMenus(id).subscribe((response: APIResponse) => {
+      if (response.success) {
+        this.menus = response.data;
+        this.cdr.detectChanges();
+      }
+    }, (error: any) => {
+      console.error(error);
+      this.swal.fire("error", "ERROR", "Error retrieving data... try later", "");
+    });
   }
 
   handleSearchbarLocation(event: any) {
@@ -60,11 +88,10 @@ export class HomepageComponent implements AfterContentInit {
 
     this.roleService.getNearestRestaurants(latitude, longitude).subscribe((response: APIResponse) => {
       if (response.success) {
-        console.log('Received nearest restaurants data:', response.data);
         this.nearestRestaurantsList = [...response.data];
         this.showNearestRestaurants = true;
         this.circular = false;
-        this.cdr.detectChanges();
+        this.cdr.detectChanges(); 
       }
     }, (error: any) => {
       this.swal.fire("error", "Error", error.toString(), "");
@@ -75,41 +102,47 @@ export class HomepageComponent implements AfterContentInit {
     this.nearestRestaurantsList = [];
     this.showNearestRestaurants = false;
     this.circular = true;
-    this.cdr.detectChanges();
+    this.cdr.detectChanges(); // Trigger change detection after clearing search
   }
 
-  handleCardBtnClick(event: any){
-    console.log(event);
+  handleCardBtnClick(event: any) {
     this.router.navigate(['/restaurant', event]);
   }
 
-  handleSearchedText(event: string){
+  handleSearchedText(event: string) {
     if (event.length > 0) {
       this.searchRestaurants(event);
       this.handleSearchbarClear();
     }
   }
 
-  searchRestaurants(toSearch: string){
-    this.roleService.searchRestaurants(toSearch)
-      .subscribe((response: APIResponse) => {
-        if (response.success) {
-          this.nearestRestaurantsList = [...response.data];
-          this.showNearestRestaurants = true;
-          this.circular = false;
-        }
-      }, (error: any) => {
-        console.error(error);
-        this.swal.fire("error","ERROR","Error retrieving data...try later", "");
-      });
+  searchRestaurants(toSearch: string) {
+    this.roleService.searchRestaurants(toSearch).subscribe((response: APIResponse) => {
+      if (response.success) {
+        this.nearestRestaurantsList = [...response.data];
+        this.showNearestRestaurants = true;
+        this.circular = false;
+        this.cdr.detectChanges(); // Trigger change detection after updating lists
+      }
+    }, (error: any) => {
+      console.error(error);
+      this.swal.fire("error", "ERROR", "Error retrieving data... try later", "");
+    });
   }
-  getAllMenus(id: number){
-    return this.roleService.getAllMenus(id)
-      .subscribe((response: APIResponse) => {
-        response.success ? this.menus = response.data : null
-      },(error: any) => {
-        console.error("error"),
-        this.swal.fire("error","ERROR","Error retrieving data... try later", "")
-      })
+  openModal() {
+    this.modalService.open(UpdateModalComponent, {restaurant: this.restaurantsList}, "UPDATE MODAL")
   }
+  
+  getRestaurantReservations(id: number){
+      this.roleService.getRestaurnatReservations(id)
+        .subscribe((response: APIResponse) => {
+          response.success ? this.reservations = response.data : null
+          
+        },(error: any ) => {
+          console.error(error);
+        })
+  }
+
+
+
 }
